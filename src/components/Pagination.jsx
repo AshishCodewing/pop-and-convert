c97295a166628ce11074e39bf6d1d2f0833331d5
Ashish Shrestha <ashish11stha.codewing@gmail.com>
2023-07-12 15:36:52 +0545

    adds: pagination active style

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,14 +1,17 @@
-function Pagination({notificationPerPage, totalNotification, handlePageChange}) {
+function Pagination({ notificationPerPage, totalNotification, handlePageChange, currentPage }) {
     return (
-        <ul className="flex gap-1">
-            {Array.from({length: Math.ceil(totalNotification / notificationPerPage)}, (_, i) => {
-                return (
-                    <li key={i}>
-                        <button className="border p-3 border-primaryAccent" onClick={() => handlePageChange(i + 1)} >{i+ 1}</button>
-                    </li>
-                )
-            })}
-        </ul>
+        <>
+            <ul className="flex gap-1">
+                {Array.from({ length: Math.ceil(totalNotification / notificationPerPage) }, (item, index) => {
+                    return (
+                        <li key={index}>
+                            <button className={`border p-3 border-primaryAccent ${currentPage === index + 1 && 'bg-borderLight'}`} onClick={() => handlePageChange(index + 1)} >{index + 1}</button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+
     )
 }
 
